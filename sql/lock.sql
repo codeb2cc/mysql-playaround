af850b38b6b9997db16739a28989bfb4924913f5
Codeb Fan <codeb2cc@gmail.com>
2012-08-22 17:02:01 +0800

    Dig into Next-key lock

--- sql/lock.sql
+++ sql/lock.sql
@@ -46,13 +46,13 @@ START TRANSACTION;
 ---- Before Client B insert new record
 SELECT * FROM lock_test WHERE value > 8;    -- No result
 ---- After Client B commit
-SELECT * FROM lock_test;
+SELECT * FROM lock_test WHERE value > 8;    -- Still no result
 DELETE FROM lock_test WHERE value > 8;      -- 1 row affected !!
 ROLLBACK;
 ---- The same but use a locking read
 START TRANSACTION;
 SELECT * FROM lock_test WHERE value > 10;    -- No result
-SELECT * FROM lock_test WHERE value > 10 LOCK IN SHARE MODE;    -- Surprise
+SELECT * FROM lock_test WHERE value > 10 LOCK IN SHARE MODE;    -- Surprise !
 ROLLBACK;
 
 --- Client B
@@ -72,15 +72,32 @@ COMMIT;
 -------------------------------------------------------------------------------
 
 -- Next-key lock example
--- Client A
+--- Client A
+---- Test 1
 START TRANSACTION;
-SELECT * FROM lock_test WHERE value > 8 FOR UPDATE;     -- Next-key lock
+SELECT * FROM lock_test WHERE value > 5 FOR UPDATE;     -- Next-key lock(gap, 8 ... max, gap)
+ROLLBACK;
+---- Test 2
+START TRANSACTION;
+SELECT * FROM lock_test WHERE id > 5 FOR UPDATE;
 ROLLBACK;
 
--- Client B
+--- Client B
+---- Test 1
 START TRANSACTION;
----- After Client A query
-INSERT INTO lock_test (value, message) VALUES(8, 'Ops');  -- Lock timeout
+SELECT * FROM lock_test;
+---- After Client A SELECT ... FOR UPDATE statement
+UPDATE lock_test SET message = 'Ops' WHERE value = 9;      -- Record lock in Next-key lock
+INSERT INTO lock_test (value, message) VALUES(6, 'Ops');   -- Gap lock in Next-key lock
+INSERT INTO lock_test (value, message) VALUES(4, 'Ops');   -- Gap lock?
+INSERT INTO lock_test (value, message) VALUES(2, 'Ops');   -- Lock too and why?
+ROLLBACK;
+---- Test 2
+START TRANSACTION;
+INSERT INTO lock_test (value, message) VALUES(2, 'Ops');   -- Lock
+INSERT INTO lock_test (id, value, message) VALUES(2, 2, 'Bingo');   -- Success !
+INSERT INTO lock_test (id, value, message) VALUES(7, 2, 'Ops');   -- Failed
+---- Next-key in id index !!
 ROLLBACK;
 
 -------------------------------------------------------------------------------
